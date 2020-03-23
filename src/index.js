2051cb579cb9958735cdde247d0ce70316cf2ce2
Selma Datagirl <datagirlcmr@gmail.com>
2020-03-23 12:21:18 -0500

    buildt the markup of the restaurant page using html

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles/style.css';
+import 'bootstrap';
+import  'bootstrap/dist/css/bootstrap.min.css';
+
+
+// const content = document.querySelector("#content");
+// content.innerHTML = "Hey you";
+// console.log (content);
+
